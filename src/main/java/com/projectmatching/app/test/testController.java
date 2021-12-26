aa98d83cc205f0c3ca64f14c23815a43f413b988
philos1234 <48795102+philos1234@users.noreply.github.com>
2021-12-26 09:05:14 +0900

    testController

--- src/main/java/com/projectmatching/app/test/testController.java
+++ src/main/java/com/projectmatching/app/test/testController.java
@@ -1,10 +1,23 @@
 package com.projectmatching.app.test;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class testController {
+
+
+
+    @GetMapping("/")
+    public String test3(@RequestBody testData data) {
+
+        System.out.println(data.getTest());
+        System.out.println(data.getArr());
+        return "test api has been returned!\n";
+
+    }
+
 
     @GetMapping("/test")
     public String test() {
---
+++ src/main/java/com/projectmatching/app/test/testData.java
@@ -0,0 +1,16 @@
+package com.projectmatching.app.test;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter @Setter
+@AllArgsConstructor
+public class testData {
+
+    private String test;
+    private List<Integer> arr;
+
+}
