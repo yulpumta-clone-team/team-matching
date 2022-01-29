701bb3ac1e3edcbfb4da33c3ea61306c7955e38b
philos1234 <48795102+philos1234@users.noreply.github.com>
2022-01-30 00:49:38 +0900

    일반 회원가입 기능 밒 validation 추가

--- src/main/java/com/projectmatching/app/domain/user/dto/UserLoginDto.java
+++ src/main/java/com/projectmatching/app/domain/user/dto/UserLoginDto.java
@@ -1,43 +1,22 @@
 package com.projectmatching.app.domain.user.dto;
 
-import com.fasterxml.jackson.annotation.JsonInclude;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Pattern;
 
-@AllArgsConstructor
+@Getter @Setter
 @NoArgsConstructor
-@Getter
-@Builder
-@JsonInclude(JsonInclude.Include.NON_NULL)
-public class UserDto {
+public class UserLoginDto {
 
     @Email
     private String email;
 
-    private String name;
-
     @NotEmpty
     @Pattern(regexp="(?=.*[0-9])(?=.*[a-z])(?=.*[!@#$%^&+=])(?=\\S+$).{8,20}") //8자 이상 20자 이하, 숫자 한개이상 특수문자 한개이상 포함 공백 불가
-    private String pwd;
-
-    private String portfolio;
-    private String slogan;
-    private String img;
-    private String content;//자기소개
-    
-    private String hope_session; //원하는 작업기간
-
-    private String job; //직업
-
-
-
-
-
+    private  String pwd;
 
 }
--- src/main/java/com/projectmatching/app/util/JwtAuthFilter.java
+++ src/main/java/com/projectmatching/app/util/JwtAuthFilter.java
@@ -3,24 +3,25 @@ package com.projectmatching.app.util;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.filter.GenericFilterBean;
+import org.springframework.util.AntPathMatcher;
+import org.springframework.web.filter.OncePerRequestFilter;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-
+import java.util.Collection;
+import java.util.LinkedHashSet;
 
 
 @RequiredArgsConstructor
-public class JwtAuthFilter extends GenericFilterBean {
+public class JwtAuthFilter extends OncePerRequestFilter {
 
     private final AuthTokenProvider authTokenProvider;
 
     @Override
-    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         // 헤더에서 JWT 를 받아옵니다.
         String token = authTokenProvider.resolveCookie((HttpServletRequest) request);
         // 유효한 토큰인지 확인합니다.
@@ -30,8 +31,16 @@ public class JwtAuthFilter extends GenericFilterBean {
             // SecurityContext 에 Authentication 객체를 저장합니다.
             SecurityContextHolder.getContext().setAuthentication(authentication);
         }
-        chain.doFilter(request, response);
+        filterChain.doFilter(request, response);
     }
 
+    @Override
+    protected boolean shouldNotFilter(HttpServletRequest request) throws ServletException {
+        Collection<String> excludeUrlPatterns = new LinkedHashSet<>();
+        excludeUrlPatterns.add("/login/**");
+        excludeUrlPatterns.add("/join/**");
 
+        return excludeUrlPatterns.stream()
+                .anyMatch(pattern -> new AntPathMatcher().match(pattern, request.getServletPath()));
+    }
 }
