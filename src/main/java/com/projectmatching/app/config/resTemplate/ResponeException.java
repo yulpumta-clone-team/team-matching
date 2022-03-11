945b502e9f9c4db4618d30d47a1a7e1b8242868a
philos1234 <48795102+philos1234@users.noreply.github.com>
2022-03-11 23:46:02 +0900

    resTemplate package 수정

--- src/main/java/com/projectmatching/app/config/resTemplate/ResponeException.java
+++ src/main/java/com/projectmatching/app/config/resTemplate/ResponeException.java
@@ -9,5 +9,6 @@ import lombok.Setter;
 @Setter
 @AllArgsConstructor
 public class ResponeException extends RuntimeException{
-    private ResponseTemplateStatus status;
+    private ResponseTemplateStatus responseTemplateStatus;
+
 }
