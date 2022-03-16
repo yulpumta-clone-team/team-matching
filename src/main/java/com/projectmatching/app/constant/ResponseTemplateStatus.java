ee0e26527feba574ff7705d9135211b53fe841c4
hyojeongyun <ynhyojng@gmail.com>
2022-03-16 20:29:14 +0900

    ResponseTemplateStatus 수정

--- src/main/java/com/projectmatching/app/constant/ResponseTemplateStatus.java
+++ src/main/java/com/projectmatching/app/constant/ResponseTemplateStatus.java
@@ -33,24 +33,6 @@ public enum ResponseTemplateStatus {
     /**
      * 3000: UserController  회원 가입, 로그인, 탈퇴 관련 오류
      */
-
-
-
-
-    /**
-     * 4000: TeamController 팀 관련 오류
-     */
-    SAVE_TEAM_ERROR(false, "팀 등록 실패", 4000),
-    EMPTY_TEAM_NAME(false, "팀 이름을 입력해주세요.", 4001),
-    GET_TEAMS_ERROR(false, "팀 게시글 리스트 가져오기 실패", 4002),
-    INVALID_TEAM_IDX(false, "유효하지 않은 team id입니다.", 4003),
-    DELETE_TEAM_ERROR(false, "팀 삭제하기 실패", 4004),
-    NOT_EXIST_USER(false, "존재하지 않는 유저입니다.", 4005),
-    GET_TEAM_ERROR(false, "팀 게시글 가져오기 실패", 4002),
-
-    PWD_FORM_INVALID(false,"비밀번호 형식 오류",3007)
-
-
     WITHDRAWAL_USER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR,"회원탈퇴 실패",3000),
     LOGIN_USER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR,"로그인에 실패하였습니다",3001),
     JOIN_USER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR,"회원가입 실패",3002),
@@ -59,6 +41,24 @@ public enum ResponseTemplateStatus {
     EMAIL_DUPLICATE(HttpStatus.BAD_REQUEST,"중복된 이메일입니다",3005),
     NAME_DUPLICATE(HttpStatus.BAD_REQUEST,"중복된 이름입니다.",3006),
     PWD_FORM_INVALID(HttpStatus.BAD_REQUEST,"비밀번호 형식 오류",3007)
+
+
+
+    /**
+     * 4000: TeamController 팀 관련 오류
+     */
+    ,
+    SAVE_TEAM_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "팀 등록 실패", 4000),
+    EMPTY_TEAM_NAME(HttpStatus.BAD_REQUEST, "팀 이름을 입력해주세요.", 4001),
+    GET_TEAMS_ERROR((HttpStatus.INTERNAL_SERVER_ERROR, "팀 게시글 리스트 가져오기 실패", 4002),
+    INVALID_TEAM_IDX(HttpStatus.BAD_REQUEST, "유효하지 않은 team id입니다.", 4003),
+    DELETE_TEAM_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "팀 삭제하기 실패", 4004),
+    NOT_EXIST_USER(HttpStatus.BAD_REQUEST, "존재하지 않는 유저입니다.", 4005),
+    GET_TEAM_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "팀 게시글 가져오기 실패", 4002)
+
+
+
+
 
     ,
 
